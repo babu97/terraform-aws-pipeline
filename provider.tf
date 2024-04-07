fc2b48f6b4537319c04ee8f40022d06a9638146a
babu97 <benkaimugul@gmail.com>
2024-04-07 23:55:06 +0100

    modified bucket region

--- provider.tf
+++ provider.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "cicd-class"
+    bucket = "cicd-babu"
     key    = "terraform.tfstate"
-    region = "eu-west-2"
+    region = "eu-west-1"
   }
 }
